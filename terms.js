75c8c6ab3e27e3a321bdcf2cddc9957ee92c7d81
joerverson barbosa <joerverson_santos@gmail.com>
2014-08-20 23:07:00 -0300

    alterações finais comcluida - Beta V0.01

--- terms.js
+++ terms.js
@@ -4,14 +4,4 @@ var terms = [
         take: function(){ alert('Oi, Seja Bem vindo ao HVoice.JS!!'); },
         desc: 'Boas Vindas do HVoice.JS'
     }
-, { 
-   term:'asd', 
-   take:function(){ document.location.href = 'sd'; },
-   desc: 'Levar para a Página sd'
-  }
-, { 
-   term:'asd', 
-   take:function(){ document.location.href = 'sd'; },
-   desc: 'Levar para a Página sd'
-  }
 ]
